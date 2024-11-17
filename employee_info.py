dc5e83ce9ac743670691aa78e59441c33325d6bb
kk2109 <sihuic.23@ichat.sp.edu.sg>
2024-11-17 20:51:54 +0800

    calculate_average_salary function implemented 5c

--- employee_info.py
+++ employee_info.py
@@ -21,9 +21,10 @@ def get_employees_by_age_range(age_lower_limit, age_upper_limit):
 def calculate_average_salary():
     total = 0
     average = 0
-
     #add your implementation to calculate here
-
+    for item in employee_data:
+        total += float(item['salary'])
+        average = total/ len(employee_data)
 
     return average
 
